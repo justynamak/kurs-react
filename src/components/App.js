9732b22ef1c7f4eade3340c96c20bc0254abd913
justynaMak <justynamakuch@outlook.com>
2020-01-22 11:25:15 +0100

    dodanie stanu wprowadzonych książek

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,34 @@
 import React, { Component } from "react";
+import AdminPanel from "./AdminPanel/AdminPanel";
+import Order from "./Order/Order";
+import Header from "./Header/Header";
+import Inventory from "./Inventory/Inventory";
+import "../index.css";
 
 class App extends Component {
+  state = {
+    books: []
+  };
+
+  addBook = book => {
+    const books = [...this.state.books];
+
+    books.push(book);
+
+    this.setState({
+      books
+    });
+  };
   render() {
+    const { books } = this.state;
     return (
-      <div>
-        <i>Hello world</i>
+      <div className="app container">
+        <Header />
+        <div className="row">
+          <Order />
+          <Inventory books={books} />
+          <AdminPanel addBook={this.addBook} />
+        </div>
       </div>
     );
   }
