ad8754e082b2f933ba9552f27b9b00b9517172c3
justynaMak <justynamakuch@outlook.com>
2020-01-24 10:58:55 +0100

    dodanie komponentu Footer i styli

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <p>Jan Kowalski {new Date().getFullYear()}</p>
+  </footer>
+);
+
+export default Footer;
