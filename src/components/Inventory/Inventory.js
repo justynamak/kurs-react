87ba28d9f467f8fb3be6a8a680becfb20c9d585e
justynaMak <justynamakuch@outlook.com>
2020-01-24 11:01:55 +0100

    obsługa pobierania produktów z bazy danych

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,18 +1,36 @@
 import React, { Component } from "react";
 import BookView from "../Views/BookView";
+import { fbase } from "../../fbase";
 
 class Inventory extends Component {
+  state = {
+    books: []
+  };
+  componentDidMount() {
+    this.ref = fbase.syncState("bookstore/books", {
+      context: this,
+      state: "books"
+    });
+  }
+  componentWillUnmount() {
+    fbase.removeBinding(this.ref);
+  }
   render() {
-    // const booksList = this.props.books.map(book => (
-    //   <BookView
-    //     key={book.name}
-    //     book={book}
-    //     addToOrder={this.props.addToOrder}
-    //   />
-    // ));
+    const booksList = Array.isArray(this.state.books)
+      ? this.state.books.map(book => {
+          return (
+            <BookView
+              key={book.name}
+              book={book}
+              addToOrder={this.props.addToOrder}
+            />
+          );
+        })
+      : "";
     return (
-      <div className="inventory col-md-6">
-        {/* {booksList.length ? <ul className="w-100">{booksList}</ul> : null} */}
+      <div className="offset-md-2 inventory col-md-5">
+        <h2 className="text-center mb-2">Books inventory</h2>
+        {booksList.length ? <ul className="w-100">{booksList}</ul> : null}
       </div>
     );
   }
